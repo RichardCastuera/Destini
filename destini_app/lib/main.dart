b100f3939712f56edd130e08f03cd050007f75d0
unknown <richardcastuera16@gmail.com>
2023-06-27 13:29:27 +0800

    feat(destini_app): Added Visibility function

--- destini_app/lib/main.dart
+++ destini_app/lib/main.dart
@@ -52,26 +52,28 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    //Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    setState(() {
-                      storyBrain.nextStory(1);
-                    });
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.red),
-                    foregroundColor: MaterialStatePropertyAll(Colors.white),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      style: TextStyle(
-                        fontSize: 20.0,
+                child: Visibility(
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 1 made by user.
+                      //Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(1);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.red),
+                      foregroundColor: MaterialStatePropertyAll(Colors.white),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        //Step 13 - Use the storyBrain to get the text for choice 1.
+                        storyBrain.getChoice1(),
                       ),
-                      //Step 13 - Use the storyBrain to get the text for choice 1.
-                      storyBrain.getChoice1(),
                     ),
                   ),
                 ),
@@ -81,28 +83,31 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    setState(() {
-                      storyBrain.nextStory(2);
-                    });
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
-                    foregroundColor: MaterialStatePropertyAll(Colors.white),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      style: TextStyle(
-                        fontSize: 20.0,
+                //Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                      foregroundColor: MaterialStatePropertyAll(Colors.white),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        //Step 14 - Use the storyBrain to get the text for choice 2.
+                        storyBrain.getChoice2(),
                       ),
-                      //Step 14 - Use the storyBrain to get the text for choice 2.
-                      storyBrain.getChoice2(),
                     ),
                   ),
                 ),
--- destini_app/lib/story_brain.dart
+++ destini_app/lib/story_brain.dart
@@ -4,7 +4,7 @@ import 'story.dart';
 //Step 5 - Create a new class called StoryBrain.
 class StoryBrain {
   //Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   //Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
   List<Story> _storyData = [
@@ -43,32 +43,33 @@ class StoryBrain {
 
 //Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle!;
+    return _storyData[_storyNumber].storyTitle!;
   }
 
 //Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[storyNumber].choice1!;
+    return _storyData[_storyNumber].choice1!;
   }
 
 //Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2!;
+    return _storyData[_storyNumber].choice2!;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+//Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+  // Done
 
 //Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
   void nextStory(int choiceNumber) {
-    if (choiceNumber == 1 && storyNumber == 0) {
-      storyNumber = 2;
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
 //Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
@@ -78,5 +79,12 @@ class StoryBrain {
 
 //Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+//Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
