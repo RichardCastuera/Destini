a9bf43672d6df205c6e8b66f88c80329d4988db1
unknown <richardcastuera16@gmail.com>
2023-06-27 11:48:51 +0800

    feat(destini_app): Added the first story to display

--- destini_app/lib/main.dart
+++ destini_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destini_app/story_brain.dart';
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
@@ -14,7 +15,8 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+//Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -42,11 +44,11 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
+                    //Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    storyBrain.getStory(),
                   ),
                 ),
               ),
@@ -61,11 +63,14 @@ class _StoryPageState extends State<StoryPage> {
                     backgroundColor: MaterialStatePropertyAll(Colors.red),
                     foregroundColor: MaterialStatePropertyAll(Colors.white),
                   ),
-                  child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      //Step 13 - Use the storyBrain to get the text for choice 1.
+                      storyBrain.getChoice1(),
                     ),
                   ),
                 ),
@@ -86,11 +91,14 @@ class _StoryPageState extends State<StoryPage> {
                     backgroundColor: MaterialStatePropertyAll(Colors.blue),
                     foregroundColor: MaterialStatePropertyAll(Colors.white),
                   ),
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      //Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
                     ),
                   ),
                 ),
